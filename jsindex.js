7b817cf53508f6288ad972b0b932839d06b3d37f
vaimports145 <163445944+vaimports145@users.noreply.github.com>
2024-04-02 14:07:20 -0300

    Create jsindex.js

---
+++ jsindex.js
@@ -0,0 +1,55 @@
+//_____Rolagem no celular_____//
+
+$(document).ready(function(){
+	if ($(window).width() < 700) {
+		$("#produtos").on("swipeleft", function(){
+        	$(this).animate({scrollLeft: '+=' + $(window).width()}, 'slow');
+      	});
+      
+      	$("#produtos").on("swiperight", function(){
+        	$(this).animate({scrollLeft: '-=' + $(window).width()}, 'slow');
+      	});
+      
+      	$("#tabelabolas").on("swipeleft", function(){
+       		$(this).animate({scrollLeft: '+=' + $(window).width()}, 'slow');
+      	});
+      
+      	$("#tabelabolas").on("swiperight", function(){
+        	$(this).animate({scrollLeft: '-=' + $(window).width()}, 'slow');
+      	});
+    }
+});
+
+//__________Menu__________//
+
+function openNav() {
+ 	document.getElementById("mySidenav").style.width = "50%";
+ 	var imagens = document.getElementById('slides');
+}
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+ 	var imagens = document.getElementById('slides');
+}
+
+//__________Buscar produtos__________//
+
+document.getElementById('campopesquisa').addEventListener('keyup', function(event) {
+	if (event.keyCode === 13) {
+		buscar();
+    }
+});
+    
+function buscar() {
+	var termoBusca = document.getElementById('campopesquisa').value.trim();
+	if (termoBusca !== '') {
+    	window.location.href = 'produtos.html?termo=' + encodeURIComponent(termoBusca);
+  	}
+}
+
+//__________Buscar categorias__________//
+
+function categorias() {
+    var elementoCategoria = event.target.closest('.tudojunto').querySelector('.nomecategoria');
+    var textoCategoria = elementoCategoria.innerText;
+    window.location.href = 'produtos.html?termo=' + encodeURIComponent(textoCategoria);
+}
