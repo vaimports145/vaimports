880810517bdf4d65a8152486ae0e4f4c7091b319
vaimports145 <163445944+vaimports145@users.noreply.github.com>
2024-04-02 15:17:59 -0300

    Update jsindex.js

--- jsindex.js
+++ jsindex.js
@@ -57,12 +57,12 @@ function categorias() {
 function categoriastopo() {
   var selecionado = document.getElementById("category").value;
   if (selecionado !== "") {
-    window.location.href = 'produtos.html?termo=' + selecionado;
+    window.location.href = 'produtos.html?termo=' + encodeURIComponent(selecionado);
   }
 }
 
 function categoriastopo(categoria) {
   if (categoria !== "") {
-    window.location.href = 'produtos.html?termo=' + categoria;
+    window.location.href = 'produtos.html?termo=' + encodeURIComponent(categoria);
   }
 }
