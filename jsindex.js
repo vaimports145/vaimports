976f4c0e2c5bee4dff135b3276dbd397d2557b23
vaimports145 <163445944+vaimports145@users.noreply.github.com>
2024-04-03 08:36:48 -0300

    Update jsindex.js

--- jsindex.js
+++ jsindex.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+	var listaProdutos = JSON.parse(localStorage.getItem('listaProdutos')) || [];
+    document.querySelector('.quanticar').textContent = listaProdutos.length;
+}
+
 //_____Rolagem no celular_____//
 
 $(document).ready(function(){
